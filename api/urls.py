d67dd752a461b798861e6929d3a4f7daadd31d8a
fnngj <fnngj@126.com>
2019-07-27 21:26:19 +0800

    add new test api

--- api/urls.py
+++ api/urls.py
@@ -7,5 +7,8 @@ urlpatterns = [
     path('hello_world/', views.hello_world),
     path('user/<slug:username>', views.user),
     path('get_user_info/<int:uid>', views.get_user_info),
-
+    path('search', views.search),
+    path('login', views.post_login),
+    path('add_user', views.post_add_user),
+    path('header', views.post_header),
 ]
--- api/views.py
+++ api/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+import json
 from django.http import JsonResponse
 
 
@@ -27,6 +27,91 @@ def get_user_info(request, uid):
             response = {"code": 10101, "message": "user id null"}
         else:
             response = {"code": 10200, "message": "success", "data": user_info}
+        return JsonResponse(response)
+    else:
+        return JsonResponse({"code": 10101, "message": "request method error"})
+
+
+def search(request):
+    """
+    一般GET请求
+    """
+    if request.method == "GET":
+        search_word = request.GET["q"]
+        if search_word == "selenium":
+            data_list = ["selenium教程", "seleniumhq.org", "selenium环境安装"]
+        else:
+            data_list = []
+        return JsonResponse({"code": 10200, "message": "success", "data": data_list})
+    else:
+        return JsonResponse({"code": 10101, "message": "request method error"})
+
+
+def post_login(request):
+    """
+    请求方法：POST
+    参数类型：from-data/x-www-from-urlencode
+    """
+    if request.method == "POST":
+        username = request.POST('username')
+        password = request.POST('password')
+
+        if username is None or password is None:
+            response = {"code": 10102, "message": "username or passwrord is None"}
+
+        elif username == "" or password == "":
+            response = {"code": 10203, "message": "username or passwrord is null"}
+
+        elif username == "admin" and password == "a123456":
+            response = {"code": 10200, "message": "login success"}
+
+        else:
+            response = {"code": 10104, "message": "username or password error"}
+        return JsonResponse(response)
+    else:
+        return JsonResponse({"code": 10101, "message": "request method error"})
+
+
+def post_add_user(request):
+    """
+    请求方法：POST
+    参数类型：JSON
+    """
+    if request.method == "POST":
+        try:
+            data = json.loads(request.body)
+        except json.decoder.JSONDecodeError:
+            return JsonResponse({"code": 10105, "message": "format error"})
+
+        try:
+            name = data["name"]
+            age = data["age"]
+            height = data["height"]
+        except KeyError:
+            response = {"code": 10102, "message": "key null"}
+        else:
+            if name == "":
+                response = {"code": 10103, "message": "name null"}
+            elif name == "tom":
+                response = {"code": 10104, "message": "name exist"}
+            else:
+                data = {"name": name, "age": age, "height": height}
+                response = {"code": 10200, "message": "add success", "data": data}
+        return JsonResponse(response)
+    else:
+        return JsonResponse({"code": 10101, "message": "request method error"})
+
+
+def post_header(request):
+    """
+    请求方法：POST
+    带请求提头：Header
+    """
+    if request.method == 'POST':
+        token = request.headers.get("token")
+        ct = request.headers.get("Content-Type")
+        response = {"code": 10200, "message": "header ok!",
+                    "data": {"token": token, "Content-Type": ct}}
         return JsonResponse(response)
     else:
         return JsonResponse({"code": 10101, "message": "request method error"})
